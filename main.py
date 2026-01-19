c09555791c3ed37b381b99772f673e876346873e
avencores <64781822+AvenCores@users.noreply.github.com>
2026-01-19 22:09:54 +0300

    update

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 from services import get_vpn_configs
 
-# Указываем static_folder='media', чтобы сохранить пути /media/... как в оригинале
-app = Flask(__name__, static_folder='media', static_url_path='/media')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     configs = get_vpn_configs()
     return render_template('index.html', configs=configs)
+
+@app.route('/LICENSE')
+def serve_license():
+    return send_from_directory('static', 'LICENSE')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
