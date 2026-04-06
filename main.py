bcaeeb26696d45e944386acddd78765a1c02f0a2
avencores <64781822+AvenCores@users.noreply.github.com>
2026-04-06 13:12:54 +0300

    fix

--- main.py
+++ main.py
@@ -306,6 +306,8 @@ def home():
     configs = get_vpn_configs()
     analytics_ids = get_analytics_ids()
     site_url = get_site_url()
+    # Гарантируем абсолютный URL для canonical
+    canonical_url = site_url if site_url else request.url_root.rstrip('/') + '/'
     meta_title = os.environ.get('META_TITLE', DEFAULT_META_TITLE)
     meta_description = os.environ.get('META_DESCRIPTION', DEFAULT_META_DESCRIPTION)
     meta_keywords = os.environ.get('META_KEYWORDS', DEFAULT_META_KEYWORDS)
@@ -315,7 +317,7 @@ def home():
         configs=configs,
         analytics_ids=analytics_ids,
         site_url=site_url,
-        canonical_url=site_url,
+        canonical_url=canonical_url,
         download_links=FALLBACK_LINKS,
         vc_runtime_link=VC_RUNTIME_FALLBACK,
         meta_title=meta_title,
