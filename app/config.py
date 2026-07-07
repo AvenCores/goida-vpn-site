aeb11b77797d395b6b6d33457eb6b5ae72172d67
AvenCores <avencores@gmail.com>
2026-07-07 13:32:58 +0300

    add url 26

--- app/config.py
+++ app/config.py
@@ -88,5 +88,6 @@ SOURCES_MAP = {
         "https://github.com/ByeWhiteLists/ByeWhiteLists2",
         "https://gitverse.ru/cid-uskoritel/cid-white",
         "https://github.com/zieng2/wl",
+        "https://etoneya.su/",
     ]
 }
